fba8afa8233ed00856f5433db6d828f803d9dd93
Marina Iurchenko <lamanch1203@gmail.com>
2023-04-04 15:22:35 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 2500;
+        int y = x / 100;System.out.println("Бонус");
+        System.out.println(x / 100);
+        int z = 200;
+        if (x > 1000) { System.out.println("Баланс");
+            System.out.println(x + y + z);
+
+        } else {
+            System.out.println(x + z);
+
+        }
+
+        int bonus = y;
+
+
+    }
+}
+
+
+
+
+
